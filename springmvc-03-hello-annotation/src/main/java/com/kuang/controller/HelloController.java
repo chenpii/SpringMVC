13d0ce7075e2689d6b13dbe17747dd9c93aa4203
chenh <641760512@qq.com>
2022-09-13 22:09:34 +0800

    手写03

--- springmvc-03-hello-annotation/src/main/java/com/kuang/controller/HelloController.java
+++ springmvc-03-hello-annotation/src/main/java/com/kuang/controller/HelloController.java
@@ -4,15 +4,23 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+/**
+ * @author chenpi
+ * @create 2022-09-13 21:52
+ */
 @Controller
-@RequestMapping("/HelloController")
+@RequestMapping("/hello")
 public class HelloController {
-    //真实访问地址 : 项目名/HelloController/hello
-    @RequestMapping("/hello")
-    public String sayHello(Model model){
+
+    //真实访问地址：localhost:8080/项目名/hello/h1
+    @RequestMapping("/h1")
+    public String hello(Model model) {
+
+        //封装数据
         //向模型中添加属性msg与值，可以在JSP页面中取出并渲染
-        model.addAttribute("msg","hello,SpringMVC");
-        //web-inf/jsp/hello.jsp
-        return "hello";
+        model.addAttribute("msg", "Hello,SpringMVCAnnotation");
+
+        //WEB-INF/jsp/hello.jsp
+        return "hello"; //会被视图解析器处理
     }
 }
