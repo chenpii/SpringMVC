3d3f9ae9606200079804130d08b5c082d0877e9f
chenp <641760512@qq.com>
2022-09-15 20:35:58 +0800

    restful 限定请求方式

--- springmvc-04-controller/src/main/java/com/kuang/controller/RestFulController.java
+++ springmvc-04-controller/src/main/java/com/kuang/controller/RestFulController.java
@@ -12,7 +12,7 @@ public class RestFulController {
     //原来的方式：    http://localhost:8080/myweb/add?a=1&b=2
     //Restful风格：   http://localhost:8080/myweb/add/a/b
 
-    @RequestMapping(name = "/add/{a}/{b}",method = RequestMethod.DELETE)
+    @RequestMapping(value = "/add/{a}/{b}",method = RequestMethod.POST)
     public String test(@PathVariable int a, @PathVariable int b, Model model) {
         int res = a + b;
         model.addAttribute("msg", "结果为：" + res);
