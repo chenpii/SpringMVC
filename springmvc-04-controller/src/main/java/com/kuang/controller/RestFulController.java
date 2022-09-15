892c9f9ac0937e982e87ae1ddf458f3834ea01b4
chenp <641760512@qq.com>
2022-09-15 20:29:33 +0800

    restful风格

---
+++ springmvc-04-controller/src/main/java/com/kuang/controller/RestFulController.java
@@ -0,0 +1,21 @@
+package com.kuang.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class RestFulController {
+
+    //原来的方式：    http://localhost:8080/myweb/add?a=1&b=2
+    //Restful风格：   http://localhost:8080/myweb/add/a/b
+
+    @RequestMapping(name = "/add/{a}/{b}",method = RequestMethod.DELETE)
+    public String test(@PathVariable int a, @PathVariable int b, Model model) {
+        int res = a + b;
+        model.addAttribute("msg", "结果为：" + res);
+        return "test";
+    }
+}
