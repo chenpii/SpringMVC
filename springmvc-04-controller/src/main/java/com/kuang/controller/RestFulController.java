0224e6f26f32fb82d99695386c6d0f0acebc766a
chenp <641760512@qq.com>
2022-09-16 17:00:32 +0800

    restful风格可以实现 路径复用

--- springmvc-04-controller/src/main/java/com/kuang/controller/RestFulController.java
+++ springmvc-04-controller/src/main/java/com/kuang/controller/RestFulController.java
@@ -2,10 +2,7 @@ package com.kuang.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class RestFulController {
@@ -14,9 +11,16 @@ public class RestFulController {
     //Restful风格：   http://localhost:8080/myweb/add/a/b
 
     @PostMapping(value = "/add/{a}/{b}")
-    public String test(@PathVariable int a, @PathVariable int b, Model model) {
+    public String test1(@PathVariable int a, @PathVariable int b, Model model) {
         int res = a + b;
-        model.addAttribute("msg", "结果为：" + res);
+        model.addAttribute("msg", "结果1为：" + res);
+        return "test";
+    }
+
+    @GetMapping(value = "/add/{a}/{b}")
+    public String test2(@PathVariable int a, @PathVariable int b, Model model) {
+        int res = a + b;
+        model.addAttribute("msg", "结果2为：" + res);
         return "test";
     }
 }
---
+++ springmvc-04-controller/web/a.jsp
@@ -0,0 +1,11 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<form action="${pageContext.request.contextPath}/add/1/2" method="post">
+    <input type="submit">
+</form>
+</body>
+</html>
