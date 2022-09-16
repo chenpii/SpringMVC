67b17a8c72c34fa49b8cf4007040b3bdece9673e
chenp <641760512@qq.com>
2022-09-16 15:29:04 +0800

    @PostMapping

--- springmvc-04-controller/src/main/java/com/kuang/controller/RestFulController.java
+++ springmvc-04-controller/src/main/java/com/kuang/controller/RestFulController.java
@@ -3,6 +3,7 @@ package com.kuang.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -12,7 +13,7 @@ public class RestFulController {
     //原来的方式：    http://localhost:8080/myweb/add?a=1&b=2
     //Restful风格：   http://localhost:8080/myweb/add/a/b
 
-    @RequestMapping(value = "/add/{a}/{b}",method = RequestMethod.POST)
+    @PostMapping(value = "/add/{a}/{b}")
     public String test(@PathVariable int a, @PathVariable int b, Model model) {
         int res = a + b;
         model.addAttribute("msg", "结果为：" + res);
